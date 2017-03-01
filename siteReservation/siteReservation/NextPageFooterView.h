40082b31ad23f6aee314562332f2b9649da80747
Nigel Lee <lihui8@le.com>
2017-03-01 20:38:18 +0800

    增加table 的footer类

---
+++ siteReservation/siteReservation/NextPageFooterView.h
@@ -0,0 +1,13 @@
+//
+//  NextPageFooterView.h
+//  siteReservation
+//
+//  Created by Nigel Lee on 01/03/2017.
+//  Copyright © 2017 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextPageFooterView : UIView
+
+@end
