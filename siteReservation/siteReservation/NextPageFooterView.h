72a60a809e9bd012e0b5ba6df0015db1aff445e1
Nigel Lee <lihui8@le.com>
2017-03-01 20:39:01 +0800

    网络请求页面状态

--- siteReservation/siteReservation/NextPageFooterView.h
+++ siteReservation/siteReservation/NextPageFooterView.h
@@ -7,7 +7,35 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+typedef enum
+{
+    PullNextPagePulling = 0,    //网络错误的时候
+    PullNextPageNormal,
+    PullNextPageLoading,
+    PullLastPage,
+    PullNewLastPage,
+} PullNextPageState;
 
 @interface NextPageFooterView : UIView
+{
+    UILabel *statusLabel;
+    CALayer *arrowImage;
+    UIView *arrowView;
+    UIActivityIndicatorView *activityView;
+    
+    PullNextPageState _state;
+    BOOL isLastPage;
+}
+
+@property(strong,nonatomic)UIColor * loadingTextColor;
+@property(strong,nonatomic)UIFont * loadingTextFont;
+@property(nonatomic,assign) PullNextPageState state;
+@property (nonatomic) BOOL isLastPage;
+- (void)setState:(PullNextPageState)aState;
+
+- (void)createFooterView;
+- (void)createLastFooterView;
 
 @end
